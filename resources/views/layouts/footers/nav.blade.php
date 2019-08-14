56ada1bd92928e1977ac7181e11f9e1ccfb1d125
Updivision Team <45566836+teamupdivision@users.noreply.github.com>
2019-08-14 07:22:32 +0000

    Initial commit

---
+++ resources/views/layouts/footers/nav.blade.php
@@ -0,0 +1,8 @@
+<div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-12">
+        <div class="copyright text-center text-muted">
+            &copy; {{ now()->year }} 
+            <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Updivision</a>
+        </div>
+    </div>
+</div>
